4bcc35f8d8f4888aa13803b35ba78fefb8adfe30
Akadeax <patrickqwertz@gmail.com>
2024-02-14 11:36:24 +0100

    start beep

--- Player/player_controller.gd
+++ Player/player_controller.gd
@@ -18,6 +18,8 @@ var current_fin_angle : float = -90
 @export var fin_follow_speed : float = 45
 @export var fin_follow_curve : Curve
 
+var moving_back : bool = false
+@export var moving_back_multiplier : float = 0.4
 
 # pickup var
 @export var is_holding_object : bool = false
@@ -28,26 +30,38 @@ func _physics_process(delta):
 
 	#region handle movement and animation
 	# handle input
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") && !moving_back:
 		current_move_angle -= rotation_speed * delta
 
-	elif Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("right") && !moving_back:
 		current_move_angle += rotation_speed * delta
 
-	velocity = Vector2(cos(deg_to_rad(current_move_angle)), sin(deg_to_rad(current_move_angle))) * forward_speed
+	if Input.is_action_just_pressed("back"):
+		current_move_angle += 180
+		moving_back = true
+	elif Input.is_action_just_released("back"):
+		current_move_angle -= 180
+		moving_back = false
+
+	var curr_speed := forward_speed
+	if moving_back:
+		curr_speed *= moving_back_multiplier
+
+	velocity = Vector2(cos(deg_to_rad(current_move_angle)), sin(deg_to_rad(current_move_angle))) * curr_speed
 
 	# body
-	var move_body_angle : float = current_move_angle + 90
-	rotation = deg_to_rad(move_body_angle)
+	if !moving_back:
+		var move_body_angle : float = current_move_angle + 90
+		rotation = deg_to_rad(move_body_angle)
 
-	# fin
-	var body_fin_rotation_diff := current_move_angle - current_fin_angle
-	var closeness_factor := absf(body_fin_rotation_diff) / 100
-	var follow_speed : float = fin_follow_speed * delta * fin_follow_curve.sample(closeness_factor)
+		# fin
+		var body_fin_rotation_diff := current_move_angle - current_fin_angle
+		var closeness_factor := absf(body_fin_rotation_diff) / 100
+		var follow_speed : float = fin_follow_speed * delta * fin_follow_curve.sample(closeness_factor)
 
-	current_fin_angle = move_toward(current_fin_angle, current_move_angle, follow_speed)
-	fin_holder.rotation =  deg_to_rad(current_fin_angle + 90) - deg_to_rad(move_body_angle)
-	#endregion
+		current_fin_angle = move_toward(current_fin_angle, current_move_angle, follow_speed)
+		fin_holder.rotation =  deg_to_rad(current_fin_angle + 90) - deg_to_rad(move_body_angle)
+		#endregion
 
 	move_and_slide()
 
@@ -76,9 +90,6 @@ func _process(__):
 			print(objects.size())
 			objects.sort_custom(distance_sort)
 			objects[0].pickup(self)
-			#var object = objects[0]
-			#objects[0].get_parent().remove_child(objects[0])
-			#add_child(object)
 			held_object = objects[0]
 			is_holding_object = true
 
