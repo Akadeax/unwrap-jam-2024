a70b7e30320c361b7ed1d17c6e1659de1cea8f94
SebastianMaxG <leander.flossie@gmail.com>
2024-02-15 09:48:45 +0100

    Van door closing and player handling
    waiting for fully closed van

--- PickupObjects/delivery_zone.gd
+++ PickupObjects/delivery_zone.gd
@@ -3,6 +3,18 @@ extends Area2D
 @export var deliver_player : AudioStreamPlayer
 @export var deliver_particles : GPUParticles2D
 
+var is_ending : bool = false
+var is_driving : bool = false
+
+func _process(delta):
+	if is_ending:
+		var players = get_tree().get_nodes_in_group("player")
+		for player in players:
+			player.position = position
+			player.visible = false
+			if is_driving:
+				position += (Vector2(-100, 0) * delta).rotated(rotation)
+	
 func _on_body_entered(body):
 	if body.is_in_group("pickup"):
 		if (!body.is_held):
@@ -20,14 +32,15 @@ func _on_body_exited(body):
 
 func _on_door_body_entered(body):
 	if body is PlayerController:
+		is_ending = true
 		$DoorOpen.visible = false
 		$DoorClosed.visible = true
-		#get_tree().get_first_node_in_group("cam").apply_shake(80, 80)
+		get_tree().get_first_node_in_group("cam").apply_shake(80, 80)
 		await get_tree().create_timer(1.0).timeout
 		$DoorClosed.visible = false
 		$VanClosed.visible = true
-		#get_tree().get_first_node_in_group("cam").apply_shake(80, 80)
-		#await get_tree().create_timer(1.0).timeout
-		#lerp(position,position + Vector2(-1000, 0).rotated(rotation),)
+		get_tree().get_first_node_in_group("cam").apply_shake(80, 80)
+		await get_tree().create_timer(1.0).timeout
+		
 		
 		EventBus.time_over.emit()
